8c27b97205bb411abe44e99eb0132edf3a6292fe
Subhasis Pal <palsubhasis100@gmail.com>
2024-10-02 11:38:06 +0530

    Appwrite Auth&Config setUp

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf.js"
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account= new Account(this.client);
+    }
+
+    async createAccount({name,email,password}){
+        try {
+            const userAcc= await this.account.create(ID.unique() ,name,email,password);
+
+            if(userAcc){
+                //call login MEthod
+                return this.login({email,password})
+            }else{
+                return userAcc;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailSession(email,password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUserStatus(){
+        try{
+            return await this.account.get();
+        }catch(err){
+            throw err;
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
